d649b35de85be864f29e55e72cca6e6b68e3405a
ConnorH7 <connor7797@gmail.com>
2020-05-03 17:32:51 -0700

    Add PersonalCookbook- work in progress

--- PersonalCookbook.java
+++ PersonalCookbook.java
@@ -1,1 +1,25 @@
+import java.util.*;
 
+public class PersonalCookbook extends Recipe{
+	// Variable(s)
+	ArrayList<Recipe> recipeList = new ArrayList<Recipe>(); 	// List containing all recipes for user
+	
+	// Constructors
+	PersonalCookbook(ArrayList<Recipe> recipeList){ 
+		this.recipeList = recipeList;
+	}
+	
+	// Methods
+	public void addRecipe(Recipe newRecipe){
+		recipeList.add(newRecipe);
+	}
+
+	public void removeRecipe(String recipeName){
+		recipeList.removeif(n -> n.name.equals(recipeName));	// Remove recipe with specified name from recipelist in cookbook
+	}
+
+	public Recipe getRecipe(String recipeName){
+		for()
+	}
+
+}
